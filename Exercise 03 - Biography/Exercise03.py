7f771995f720c09fec2b564cc02342bf8ff41658
collup2 <ebadsameer50@gmail.com>
2025-11-07 18:57:03 +0400

    Create Exercise03.py

---
+++ Exercise 03 - Biography/Exercise03.py
@@ -0,0 +1,4 @@
+name = input("Enter your name: ")  # asks the user to enter their name
+hometown = input("Enter your hometown name: ")  # asks the user to enter their hometown name
+age = int(input("Enter your age: "))  # asks the user to enter their age
+print("My name is", name, "\nI live in", hometown, "\nMy age is", age)

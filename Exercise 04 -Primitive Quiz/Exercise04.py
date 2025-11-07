578e56282c6ddf48dce41edc5fc1c06ad84bedb9
collup2 <ebadsameer50@gmail.com>
2025-11-07 19:11:59 +0400

    Update Exercise04.py

--- Exercise 04 -Primitive Quiz/Exercise04.py
+++ Exercise 04 -Primitive Quiz/Exercise04.py
@@ -1,5 +1,5 @@
-Capital_of_netherlands = input("What is the capital of the France? ")  # user inputs the correct answer
-if Capital_of_netherlands == "Paris":
+Capital_of_France = input("What is the capital of the France? ")  # user inputs the correct answer
+if Capital_of_France == "Paris":
     print("Correct!")  # this is the reply if the answer is correct
 else:
     print("Incorrect. The capital of the France is Paris.")
